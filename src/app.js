9d681fbded6d5ca78f1437911aec5af6f1c2d276
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-11 22:00:57 +0700

    ^ route index

--- src/app.js
+++ src/app.js
@@ -17,13 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Simple API Starter'
-  });
-});
-
-app.get('/index', controller.index);
+app.get('/', controller.index);
 
 app.use('/api/v1', api);
 
