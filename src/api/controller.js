48b3e32ab401f6d52faab26329941adbffa2f9da
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-11 22:01:24 +0700

    ^update controller

--- src/api/controller.js
+++ src/api/controller.js
@@ -1,7 +1,16 @@
 const { json } = require("express");
 
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+const { jsonResponse, errorJson } = require("../utils");
+
 module.exports = {
     index: (req, res) => {
-        res.send("Hallo Sam");
+        const fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
+
+        return jsonResponse(res, {
+            maintainer: "Rizky Sam Pratama <rizkysampratama@gmail.com>",
+            source: "https://github.com/rizama/holidays-id-api",
+        });
     }
 };
