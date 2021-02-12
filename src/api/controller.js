b4b4bcba294f9d657a5e9e63a703029934d2b591
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-02-12 17:00:47 +0700

    ^ controller public holiday

--- src/api/controller.js
+++ src/api/controller.js
@@ -1,8 +1,11 @@
-const { json } = require("express");
-
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
-const { jsonResponse, errorJson } = require("../utils");
+const { jsonResponse, errorJson, convertMonth, convertDay } = require("../utils");
+const userAgent = require("user-agents");
+const moment = require("moment");
+require("moment-timezone");
+const availableYear = ['2018', '2019', '2020', '2021', '2022', '2023', '2024'];
+
 
 module.exports = {
     index: (req, res) => {
@@ -11,6 +14,114 @@ module.exports = {
         return jsonResponse(res, {
             maintainer: "Rizky Sam Pratama <rizkysampratama@gmail.com>",
             source: "https://github.com/rizama/holidays-id-api",
+            public_holiday: {
+                endpoint: "/public/:year",
+                description: "Show Indonesia calendar public holidays by year.",
+                note: "Available 2018-2024",
+                example: fullUrl + "public/2021",
+            },
         });
+    },
+    holiday: async (req, res) => {
+        try {
+            const browser = await puppeteer.launch({
+                headless: true,
+                defaultViewport: null,
+                args: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                ]
+            });
+            const year = req.params.year;
+            if (!availableYear.includes(year)) {
+                return errorJson(res, "Sorry, year not available.", 400);
+            }
+            const page = await browser.newPage();
+            await page.setUserAgent(userAgent.toString());
+            await page.goto(`${process.env.BASE_URL}/${year}-dates`);
+            // await page.waitForSelector('.publicholidays.phgtable');
+            const content = await page.content();
+            const $ = cheerio.load(content);
+            const results = [];
+            $('#row-inner-travel > article > div.page__content > table > tbody').children('tr').each((index, el) => {
+
+                let dateMonth = $(el).find('td:nth-child(1)').text().trim();
+
+                if (!(dateMonth == "")) {
+                    if (dateMonth.includes('to')) {
+                        dateMonth = dateMonth.split('to');
+
+                        let startRange = moment(`${dateMonth[0]}${year}`, "DD MMM YYYY");
+                        let endRange = moment(`${dateMonth[1]} ${year}`, "DD MMM YYYY");
+                        let diff = startRange.diff(endRange, 'days');
+                        let cloneStartRange = startRange.clone();
+
+
+                        let arrDay = $(el).find('td:nth-child(2)').text().trim().split("to");
+                        let holiday = $(el).find('td:nth-child(3)').text().trim();
+
+                        if (diff != -1) {
+                            let range = Math.abs(diff) - 1;
+                            for (let index = 0; index < range; index++) {
+                                const fullDate = cloneStartRange.add(1, 'days');
+                                let date = fullDate.format("DD");
+                                let month = fullDate.format('MMMM');
+                                let day = fullDate.format('dddd');
+                                let title = holiday;
+
+                                let body = {};
+                                body.date = date;
+                                body.day = day;
+                                body.month = month;
+                                body.holiday = title;
+
+                                results.push(body);
+                            }
+                        }
+
+                        for (const [index, dateMonth_] of dateMonth.entries()) {
+
+                            newDateMonth = dateMonth_.trim().split(" ");
+                            let date = newDateMonth[0];
+                            let month = convertMonth(newDateMonth[1]);
+                            let day = convertDay(arrDay[index].trim());
+                            let title = holiday;
+
+                            let body = {};
+                            body.date = date;
+                            body.day = day;
+                            body.month = month;
+                            body.holiday = title;
+
+                            results.push(body);
+                        }
+
+                    } else {
+                        dateMonth = dateMonth.split(" ");
+                        let date = dateMonth[0];
+                        let month = convertMonth(dateMonth[1]);
+                        let day = convertDay($(el).find('td:nth-child(2)').text().trim());
+                        let holiday = $(el).find('td:nth-child(3)').text().trim();
+
+                        let body = {};
+                        body.date = date;
+                        body.day = day;
+                        body.month = month;
+                        body.holiday = holiday;
+
+                        results.push(body);
+                    }
+                }
+            });
+            
+            results.pop();
+
+            await browser.close();
+
+            return jsonResponse(res, results);
+
+        } catch (error) {
+            return errorJson(res, error);
+        }
     }
 };
